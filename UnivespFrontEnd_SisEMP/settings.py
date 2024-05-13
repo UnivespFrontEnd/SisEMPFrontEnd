793233e1132ba98f831f21eca173b48ae3c5e5bf
BackendUnivesp <univespbackend@gmail.com>
2024-05-12 22:55:50 -0300

    Adicionado sisempfrontend.onrender.com a ALLOWED_HOSTS

--- UnivespFrontEnd_SisEMP/settings.py
+++ UnivespFrontEnd_SisEMP/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-nlu4(qh*j44dc6by7nlrdb6#xkjd$5a3-#-y&q879szm#3r2$c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sisempfrontend.onrender.com']
 
 
 # Application definition
