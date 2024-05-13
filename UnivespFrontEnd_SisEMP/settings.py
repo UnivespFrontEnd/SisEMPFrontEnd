aa0c8b42f6328339ed470209f4a942661b6cbb64
BackendUnivesp <univespbackend@gmail.com>
2024-05-12 23:27:10 -0300

    Corrigindo STATICFILES_URL e STATICFILES_STORAGE

--- UnivespFrontEnd_SisEMP/settings.py
+++ UnivespFrontEnd_SisEMP/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 STATICFILE_DIRS = [
     os.path.join(BASE_DIR, 'static')
@@ -121,7 +121,7 @@ if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
-    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 
